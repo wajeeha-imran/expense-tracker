b97ca58090559f173c3d6eea66a2c6057ff74228
wajeeha imran <wajeehan03@gmail.com>
2026-07-06 16:27:53 +0500

    Update data-source.js

--- expense/backend/config/data-source.js
+++ expense/backend/config/data-source.js
@@ -7,13 +7,18 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
+
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 
-  entities: [Transaction],   
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
+  entities: [Transaction],
 
   synchronize: true,
   logging: false,
